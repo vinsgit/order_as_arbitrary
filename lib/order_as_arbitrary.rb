eac99fbb62b4b5d170412b1ef17dba7ebd6f5d23
admin <825461025@qq.com>
2019-11-06 05:08:25 -0800

    second version

--- lib/order_as_arbitrary.rb
+++ lib/order_as_arbitrary.rb
@@ -1,26 +1,23 @@
 require "order_as_arbitrary/version"
 require "order_as_arbitrary/error"
 module OrderAsArbitrary
-  # @param hash [Hash] the ActiveRecord arguments hash
-  # @return [ActiveRecord::Relation] the objects, ordered as specified
-
-  def method_missing(_attribute, *args)
-    _attribute = _attribute.match(/^order_as_(\w*)/)
-    if attr
-      binding.pry
-      values = *args.first
-      column_name = _attribute[1]
-      method_name = _attribute[0]
-      return all if values.empty?
-      values = values.map do |value|
-        raise OrderAsSpecified::Error, "Cannot order by `nil`" if value.nil?
-        value.is_a? Range ? range_clause(value) : quote(value)
+  def method_missing(name, *args)
+    method_name = get_method_name(name)
+    if method_name
+      check_arguments(args)
+      if args.first.is_a? Range
+        values = range_clause(args.first)
+      elsif args.first.is_a? Array
+        values = array_clause(args.first)
+      else
+        raise OrderAsArbitrary::Error, "Only accept array or range"
       end
-      self.class.send(:define_method, "#{method_name}") do |column_name, values|
+      self.class.send(:define_method, "#{method_name[0]}") do |values|
+        column_name = get_method_name(__method__)[1]
         sanitized_values_string = values.map {|value| connection.quote(value)}.join(",")
-        where("#{column_name}=?", values).order("FIELD(#{column_name}, #{sanitized_values_string})")
+        where("#{column_name} in (?)", values).order("FIELD(#{column_name}, #{sanitized_values_string})")
       end
-      send(method_name, column_name, values)
+      send(method_name[0], values)
     else
       super
     end
@@ -28,9 +25,25 @@ module OrderAsArbitrary
 
   private
 
+  def check_arguments(args)
+    raise OrderAsArbitrary::Error, "Wrong number of arguments "  if args.empty? || args.first.nil?
+  end
+
+  def get_method_name(name)
+    name.to_s.match(/^order_as_(\w*)/)
+  end
+
   def range_clause(range)
-    raise OrderAsSpecified::Error, "Range needs to be increasing" if range.first >= range.last
+    raise OrderAsArbitrary::Error, "Range needs to be increasing" if range.first >= range.last
     range.to_a
+  end
+
+  def array_clause(array)
+    array.map do |value|
+      raise OrderAsSpecified::Error, "Cannot order by `nil`" if value.nil?
+      value
+    end
+    array
   end
 
 end
